c3c5b7ee3159a853879073e6df16878d491a62d1
Marc Duiker <marcduiker@gmail.com>
2017-10-03 13:42:46 +0200

    Added console app to upload images to blobstorage. Started Azure Function app to analyze image.

---
+++ src/ImageAnalysisApp/ImageAnalysisApp/ComputerVIsionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace ImageAnalysisApp
+{
+    public class ComputerVisionHandler
+    {
+        protected const string Region = "westeurope";
+        protected const string ApiKey = "";
+        protected const string BaseCognitiveServicesVisionUrlV1 = ".api.cognitive.microsoft.com/vision/v1.0/";
+        protected string ApiEndPoint;
+        private readonly HttpClient _httpClient;
+
+        public ComputerVisionHandler()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", $"{ ApiKey }");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["visualFeatures"] = "Description, Tags, Color, Faces";
+            queryString["language"] = "en";
+            ApiEndPoint = $"https://{ Region }{ BaseCognitiveServicesVisionUrlV1 }analyze?{ queryString }";
+
+        }
+
+        public async Task<JToken> AnalyzeImage(byte[] image)
+        {
+            using (var content = new ByteArrayContent(image))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                HttpResponseMessage response = await _httpClient.PostAsync(ApiEndPoint, content);
+                Task<string> stringResponse = response.Content.ReadAsStringAsync();
+
+                return JToken.Parse(stringResponse.Result);
+            }
+        }
+    }
+}
---
+++ src/ImageUploader/ImageUploader/FileUploader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace ImageUploader
+{
+    public class FileUploader
+    {
+        private readonly CloudBlobClient _blobClient;
+        private readonly CloudBlobContainer _imagesBlobContainer;
+        private readonly CloudQueueClient _queueClient;
+
+        public FileUploader()
+        {
+            var storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            _blobClient = storageAccount.CreateCloudBlobClient();
+            _imagesBlobContainer = _blobClient.GetContainerReference(CloudConfigurationManager.GetSetting("BlobStorageContainer"));
+            _queueClient = storageAccount.CreateCloudQueueClient();
+        }
+
+        public void Upload(FileInfo file)
+        {
+            var blobName = UploadToBlobStorage(file);
+            if (IsBlobUploaded(blobName))
+            {
+                PushBlobInfoToQueue(blobName);
+            }
+        }
+
+        private string UploadToBlobStorage(FileInfo file)
+        {
+            CloudBlockBlob blockBlob = _imagesBlobContainer.GetBlockBlobReference(file.Name);
+
+            using (var fileStream = file.OpenRead())
+            {
+                blockBlob.UploadFromStream(fileStream);
+            }
+
+            return file.Name;
+        }
+
+        private bool IsBlobUploaded(string blobName)
+        {
+            var blob = _imagesBlobContainer.GetBlockBlobReference(blobName);
+
+            return blob.Exists();
+        }
+
+        private void PushBlobInfoToQueue(string blobInfo)
+        {
+            var imagesToProcessQueue = _queueClient.GetQueueReference(CloudConfigurationManager.GetSetting("QueueName"));
+            var message = new CloudQueueMessage(blobInfo);
+            imagesToProcessQueue.AddMessage(message);
+        }
+    }
+}
