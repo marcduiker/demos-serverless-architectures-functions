c2ca9638a5309931a400210dd201bbc3b3d790d5
Marc Duiker <marcduiker@gmail.com>
2017-10-25 22:55:05 +0200

    Attempt to add unit test project

---
+++ src/ImageAnalysisApp/ImageAnalysisApp.UnitTests/Functions/ValidateBlobSizeTests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using ImageAnalysisApp.Functions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace ImageAnalysisApp.UnitTests.Functions
+{
+    public class ValidateBlobSizeTests
+    {
+        [Fact]
+        public void UnitOfWork_StateUnderTest_ExpectedBehaviour()
+        {
+            // Arrange
+            string blobName = "test";
+            Stream blobStream = null;
+            Mock<ICollector<string>> imagesTooLargeQueue = GetMockedQueue();
+            Mock<ICollector<string>> imagesToProcessQueue = GetMockedQueue();
+            Mock<ILogger> logger = GetMockedLogger();
+
+            // Act
+            ValidateBlobSize.Run(
+                blobName,
+                blobStream,
+                imagesTooLargeQueue.Object,
+                imagesToProcessQueue.Object,
+                logger.Object);
+
+            // Assert
+
+        }
+
+        private Mock<ICollector<string>> GetMockedQueue()
+        {
+            var mockQueue = new Mock<ICollector<string>>();
+
+            return mockQueue;
+        }
+
+        private Mock<ILogger> GetMockedLogger()
+        {
+            var mockLogger = new Mock<ILogger>();
+
+            return mockLogger;
+        }
+    }
+}
